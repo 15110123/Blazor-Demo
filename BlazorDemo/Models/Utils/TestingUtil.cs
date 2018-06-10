4f2a7b60ce4e3c731740a8f3f70147c7dcb7a26b
15110123 <leminhtanvatc@yahoo.com.vn>
2018-06-10 17:39:05 +0700

    Đưa demo vào GitHub

---
+++ BlazorDemo/Models/Utils/TestingUtil.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlazorDemo.Models.Utils
+{
+    public static class TestingUtil
+    {
+        public static void Out(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
